5395f25804c88a1b5b364a9e6175792e16a04baf
rojandahal <maxpanye60@gmail.com>
2023-07-10 10:14:06 +0545

    Teacher filter accordion added

--- components/Screens/Routine.jsx
+++ components/Screens/Routine.jsx
@@ -3,13 +3,30 @@ import React, { useState } from "react";
 import { FlatList } from "react-native";
 import RoutineCard from "../Card/RoutineCard";
 import { useRoute } from "@react-navigation/native";
-import { Avatar, Chip } from "react-native-paper";
+import { Avatar, Chip, List } from "react-native-paper";
+import TeacherAccordion from "../TeacherAccordion/TeacherAccordion";
 
 const Routine = () => {
   const route = useRoute();
   const data = route.params.data;
   const [selectedSubject, setSelectedSubject] = useState(false);
   const [selectedBatch, setSelectedBatch] = useState(false);
+  const [expanded, setExpanded] = useState(true);
+
+  const teacherData = [
+    {
+      id: 1,
+      name: "Teacher 1",
+    },
+    {
+      id: 2,
+      name: "Teacher 2",
+    },
+  ];
+
+  const _handlePress = () => {
+    setExpanded(!expanded);
+  };
 
   // console.log(data)
   return (
@@ -41,6 +58,7 @@ const Routine = () => {
           </Chip>
         </View>
       </View>
+      <TeacherAccordion teacherData={teacherData} />
       <FlatList
         data={data}
         renderItem={({ item }) => <RoutineCard data={item} />}
---
+++ components/TeacherAccordion/TeacherAccordion.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { FlatList } from "react-native";
+import { Chip, List } from "react-native-paper";
+
+const TeacherAccordion = ({ teacherData }) => {
+  const [selectedTeacher, setSelectedTeacher] = useState(null);
+
+  useEffect(() => {
+    console.log(selectedTeacher);
+  }, [selectedTeacher]);
+
+  const handleTeacherSelection = teacherName => {
+    setSelectedTeacher(teacherName);
+  };
+
+  return (
+    <List.Section>
+      <List.Accordion
+        left={props => <List.Icon {...props} />}
+        title='Select Teacher'
+      >
+        <FlatList
+          data={teacherData}
+          renderItem={({ item }) => (
+            <Chip
+              mode='outlined'
+              style={{ margin: 4 }}
+              onPress={() =>
+                selectedTeacher
+                  ? handleTeacherSelection(null)
+                  : handleTeacherSelection(item.name)
+              }
+              selected={selectedTeacher === item.name}
+            >
+              {item.name}
+            </Chip>
+          )}
+          keyExtractor={item => item.id}
+        />
+      </List.Accordion>
+    </List.Section>
+  );
+};
+
+export default TeacherAccordion;
